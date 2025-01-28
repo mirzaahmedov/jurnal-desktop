3dab6eedc9948613bccd09f77e2bca86960d4430
Begzod Mirzaahmedov <U0154@so.local>
2025-01-28 16:46:07 +0500

    updates

---
+++ src/renderer/src/demo.js
@@ -0,0 +1,270 @@
+// const response = {
+//   message: 'get smeta successfully',
+//   meta: {
+//     pageCount: 1,
+//     count: 25,
+//     currentPage: 1,
+//     nextPage: null,
+//     backPage: null
+//   },
+//   data: [
+//     {
+//       id: 1558,
+//       smeta_name: 'Ish haqi va to`lovlar',
+//       smeta_number: '4110000',
+//       father_smeta_name: '1.1.0.0',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1559,
+//       smeta_name: 'Pul shaklidagi ish haqi',
+//       smeta_number: '4111000',
+//       father_smeta_name: '1.1.1.0',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1560,
+//       smeta_name: 'Pul shaklidagi ish haqi',
+//       smeta_number: '4111000',
+//       father_smeta_name: '1.1.1.1',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1561,
+//       smeta_name: 'Asosiy ish haqi',
+//       smeta_number: '4111100',
+//       father_smeta_name: '1.1.1.2',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1562,
+//       smeta_name: 'Ish haqiga ustama va qo‘shimcha to‘lovlar',
+//       smeta_number: '4111200',
+//       father_smeta_name: '1.1.1.3',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1563,
+//       smeta_name:
+//         'Umumta’lim, o‘rta maxsus, professional ta’lim muassasalarining o‘rnak ko‘rsatgan xodimlarini rag‘batlantirishning Direktor jamg‘armasi mablag‘lari',
+//       smeta_number: '4111210',
+//       father_smeta_name: '1.1.1.4',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1564,
+//       smeta_name: 'Tibbiyot tashkilotlari xodimlariga ustama va qo‘shimchalar',
+//       smeta_number: '4111220',
+//       father_smeta_name: '1.1.1.5',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1565,
+//       smeta_name:
+//         'Maktabgacha ta’lim tashkilotlari xodimlarini moddiy rag‘batlantirish jamg‘armasidan to‘lovlar',
+//       smeta_number: '4111230',
+//       father_smeta_name: '1.1.1.6',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1566,
+//       smeta_name: 'Ilmiy darajaga ega bo‘lgan xodimlarga qo‘shimcha to‘lovlar',
+//       smeta_number: '4111240',
+//       father_smeta_name: '1.1.1.7',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1567,
+//       smeta_name: 'Nafaqalar',
+//       smeta_number: '4711100',
+//       father_smeta_name: '1.1.1.8',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1568,
+//       smeta_name: 'Ishsizlik bo‘yicha nafaqa',
+//       smeta_number: '4711110',
+//       father_smeta_name: '1.1.1.9',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1569,
+//       smeta_name: 'Vaqtincha mehnatga qobiliyatsizlik nafaqasi',
+//       smeta_number: '4711120',
+//       father_smeta_name: '1.1.1.10',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1570,
+//       smeta_name: 'Bola tug‘ilgani uchun nafaqa',
+//       smeta_number: '4711130',
+//       father_smeta_name: '1.1.1.11',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1571,
+//       smeta_name: 'Dafn marosimi uchun nafaqa',
+//       smeta_number: '4711140',
+//       father_smeta_name: '1.1.1.12',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1572,
+//       smeta_name: 'Homiladorlik va tug‘ish bo‘yicha nafaqa',
+//       smeta_number: '4711150',
+//       father_smeta_name: '1.1.1.13',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1573,
+//       smeta_name:
+//         'Nogironligi bo‘lgan bolasini tarbiyalayotgan ishlovchi ota-onaning biriga (vasiyga, homiyga) bola o‘n sakkiz yoshga to‘lgunga qadar oyiga qo‘shimcha bir dam olish kuni uchun nafaqa',
+//       smeta_number: '4711160',
+//       father_smeta_name: '1.1.1.14',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1574,
+//       smeta_name: 'Bolalikdan nogironligi bo‘lgan shaxslarga nafaqa',
+//       smeta_number: '4711170',
+//       father_smeta_name: '1.1.1.15',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1575,
+//       smeta_name:
+//         'Pensiya tayinlash uchun zarur bo‘lgan ish stajiga ega bo‘lmagan shaxslarga va mehnatga layoqatsizlarga nafaqa',
+//       smeta_number: '4711180',
+//       father_smeta_name: '1.1.1.16',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1576,
+//       smeta_name: 'Pensiyalar',
+//       smeta_number: '4711400',
+//       father_smeta_name: '1.1.1.17',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1577,
+//       smeta_name: 'Davlat pensiyalari va boshqa to‘lovlar',
+//       smeta_number: '4711410',
+//       father_smeta_name: '1.1.1.18',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1578,
+//       smeta_name: 'Davlat budjetidan pensiya to‘lovlari',
+//       smeta_number: '4711420',
+//       father_smeta_name: '1.1.1.19',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1579,
+//       smeta_name:
+//         'Ishlamaydigan onalarga, shuningdek budjet tashkilotlarida ishlaydigan onalarga bola ikki yoshga to‘lgunga qadar uni parvarishlash uchun beriladigan oylik nafaqa',
+//       smeta_number: '4721100',
+//       father_smeta_name: '1.1.1.20',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1580,
+//       smeta_name: 'Voyaga yetmagan 16 (18) yoshgacha bolalari bo‘lgan oilalarga nafaqalar',
+//       smeta_number: '4721200',
+//       father_smeta_name: '1.1.1.21',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1581,
+//       smeta_name: 'Kam ta’minlangan oilalarga moddiy yordam',
+//       smeta_number: '4721300',
+//       father_smeta_name: '1.1.1.22',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1582,
+//       smeta_name: 'Uy-joy-kommunal xizmatlar bo‘yicha har oylik kompensatsiya to‘lovlari',
+//       smeta_number: '4721500',
+//       father_smeta_name: '1.1.1.23',
+//       group_number: '1'
+//     },
+//     {
+//       id: 1583,
+//       smeta_name: 'Stipendiyalar',
+//       smeta_number: '4821400',
+//       father_smeta_name: '1.1.1.24',
+//       group_number: '1'
+//     }
+//   ]
+// }
+
+// const removeTrailingZeros = (str: string) => {
+//   return str.replace(/(\.0)+$/, '')
+// }
+
+// const arrayStartsWith = (arr: number[], prefix: number[]) => {
+//   return prefix.every((v, i) => v === arr[i])
+// }
+
+// export { removeTrailingZeros, arrayStartsWith }
+
+// import type { Smeta } from '@renderer/common/models'
+
+// type TreeNode = Smeta & {
+//   _levels: number[]
+//   _included?: boolean
+//   children: TreeNode[]
+// }
+
+// const treeFromArray = (list: (Smeta & { _included?: boolean })[]) => {
+//   let minLevel = Infinity
+
+//   const normalized = list.map((item) => {
+//     item._included = false
+
+//     const levels = removeTrailingZeros(item.father_smeta_name).split('.').map(Number)
+//     if (levels.length < minLevel) {
+//       minLevel = levels.length
+//     }
+
+//     return {
+//       ...item,
+//       children: [],
+//       _levels: levels
+//     }
+//   })
+
+//   const findChildren = (parent: TreeNode) => {
+//     const nodes = normalized.filter(
+//       (item) =>
+//         arrayStartsWith(item._levels, parent._levels) && item._levels.length > parent._levels.length
+//     )
+
+//     nodes.forEach(findChildren)
+
+//     const children = nodes.filter((item) => {
+//       if (!item._included) {
+//         item._included = true
+//         return true
+//       }
+//       return false
+//     })
+
+//     parent.children = children
+//   }
+
+//   const nodes = normalized.filter((item) => {
+//     if (item._levels.length === minLevel) {
+//       item._included = true
+//       return true
+//     }
+//     return false
+//   })
+
+//   nodes.forEach(findChildren)
+//   nodes.push(...normalized.filter((item) => !item._included))
+
+//   return nodes
+// }
+
+// treeFromArray(response.data)
