ca55ba4b25f2b1f0a6eecd1dfee12b9c5ff72f5d
Mirzaahmedov Bekzod <mirzaahmedov.dev@gmail.com>
2025-09-22 12:45:12 +0500

    updates

---
+++ src/renderer/src/app/_demo/components/input.tsx
@@ -0,0 +1,98 @@
+import type { FC, MutableRefObject, RefCallback } from 'react'
+
+import { cva } from 'class-variance-authority'
+import { NumericFormat } from 'react-number-format'
+
+import { cn } from '@/common/lib/utils'
+
+export interface EditorContext {
+  arrayName: string
+}
+export interface EditorProps {
+  isNumeric?: boolean
+  disabled?: boolean
+  inputRef?: MutableRefObject<HTMLInputElement> | RefCallback<HTMLInputElement>
+  context: EditorContext
+  value: unknown
+  onValueChange: (value: unknown) => void
+  onKeyDown?: (e: KeyboardEvent) => void
+  onBlur?: () => void
+  rowIndex: number
+  className?: string
+}
+
+export const EditorInput: FC<EditorProps> = (props) => {
+  const {
+    isNumeric,
+    value,
+    onValueChange,
+    onBlur,
+    className,
+    inputRef,
+    disabled,
+    rowIndex,
+    onKeyDown
+  } = props
+  return isNumeric ? (
+    <NumericFormat
+      getInputRef={(ref) => {
+        ref?.focus()
+        if (typeof inputRef === 'function') {
+          inputRef(ref)
+          return
+        }
+        if (inputRef) {
+          inputRef.current = ref
+        }
+      }}
+      disabled={disabled}
+      value={!isNaN(Number(value)) ? Number(value) || null : 0}
+      onValueChange={(values) => {
+        onValueChange(values.floatValue ?? 0)
+      }}
+      onBlur={onBlur}
+      onKeyDown={(e) => onKeyDown?.(e.nativeEvent)}
+      className={cn(inputVariants({ disabled }), className)}
+      data-rowindex={rowIndex}
+      thousandSeparator=" "
+      decimalSeparator=","
+      decimalScale={2}
+    />
+  ) : (
+    <input
+      ref={(ref) => {
+        ref?.focus()
+        if (typeof inputRef === 'function') {
+          inputRef(ref)
+          return
+        }
+        if (inputRef) {
+          inputRef.current = ref!
+        }
+      }}
+      disabled={disabled}
+      type="text"
+      value={value as string}
+      onChange={(e) => onValueChange(e.target.value)}
+      onBlur={onBlur}
+      onKeyDown={(e) => onKeyDown?.(e.nativeEvent)}
+      className={cn(inputVariants({ disabled }), className)}
+      data-rowindex={rowIndex}
+    />
+  )
+}
+
+const inputVariants = cva(
+  cn(
+    'ag-input-field-input ag-text-field-input',
+    'h-full border border-gray-300 rounded px-2 py-1 text-sm focus:outline-none focus:ring-2 focus:ring-brand'
+  ),
+  {
+    variants: {
+      disabled: {
+        true: 'bg-gray-100 text-gray-400',
+        false: 'bg-white'
+      }
+    }
+  }
+)
--- src/renderer/src/app/_demo/page.tsx
+++ src/renderer/src/app/_demo/page.tsx
@@ -1,135 +1,57 @@
-// import { PDFViewer } from '@react-pdf/renderer'
-// import { usePodpis } from '@/common/features/podpis'
-// import { parseDate } from '@/common/lib/date'
-// import { PodpisTypeDocument } from '@/common/models'
-// import { useMainSchetQuery } from '../region-spravochnik/main-schet/use-main-schet-query'
-// import { workTrip } from './demo'
-// import { WorkTripReport } from './work-trip/work-trip'
-// const DemoPage = () => {
-//   const mainSchetQuery = useMainSchetQuery()
-//   const podpis = usePodpis(PodpisTypeDocument.BANK_RASXOD_PORUCHENIYA)
-//   return (
-//     <div className="flex-1">
-//       <PDFViewer className="w-full h-full">
-//         <WorkTripReport
-//           provodki={workTrip.childs ?? []}
-//           roads={workTrip.road ?? []}
-//           workerPosition={workTrip.worker.position}
-//           workerRank={workTrip.worker.rank ?? ''}
-//           workerFIO={workTrip.worker.name}
-//           regionName={mainSchetQuery.data?.data?.tashkilot_nomi ?? ''}
-//           podpis={podpis}
-//           year={parseDate(workTrip.doc_date).getFullYear()}
-//         />
-//       </PDFViewer>
-//     </div>
-//   )
-// }
-// export default DemoPage
-import { FC, InputHTMLAttributes } from 'react'
-
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'
-import { AgGridReact, CustomCellEditorProps } from 'ag-grid-react'
-import { useFieldArray, useForm } from 'react-hook-form'
-import { Controller } from 'react-hook-form'
+import { AgGridReact } from 'ag-grid-react'
+import { FormProvider, useFieldArray, useForm } from 'react-hook-form'
+
+import { formatNumber } from '@/common/lib/format'
+
+import { NumberEditor } from './components/number-editor'
+import { TextEditor } from './components/text-editor'
+import { bankRasxodData } from './data'
 
 ModuleRegistry.registerModules([AllCommunityModule])
 
 const DemoPage = () => {
   const form = useForm({
-    defaultValues: {
-      childs: [
-        { make: 'Toyota', model: 'Celica', price: 35000 },
-        { make: 'Ford', model: 'Mondeo', price: 32000 },
-        { make: 'Porsche', model: 'Boxster', price: 72000 }
-      ]
-    }
+    defaultValues: bankRasxodData
   })
 
-  const data = useFieldArray({
+  const dataSource = useFieldArray({
     control: form.control,
     name: 'childs'
   })
 
   return (
     <div className="flex-1">
-      <AgGridReact
-        className="ag-theme-alpine h-full"
-        rowData={data.fields}
-        getRowId={(params) => params.data.id}
-        columnDefs={[
-          {
-            field: 'make',
-            editable: true,
-            cellEditor: RHFCellEditor,
-            cellRendererParams: { form },
-            cellRenderer: RHFCellEditor,
-            cellEditorParams: { form }
-          },
-          {
-            field: 'model',
-            editable: true,
-            cellEditor: RHFCellEditor,
-            cellRendererParams: { form },
-            cellRenderer: RHFCellEditor,
-            cellEditorParams: { form }
-          },
-          {
-            field: 'price',
-            editable: true,
-            cellEditor: RHFCellEditor,
-            cellRendererParams: { form },
-            cellRenderer: RHFCellEditor,
-            cellEditorParams: { form }
-          }
-        ]}
-        defaultColDef={{ flex: 1, minWidth: 100 }}
-      />
-    </div>
-  )
-}
-
-export interface TextEditorProps extends InputHTMLAttributes<HTMLInputElement> {
-  value: string
-  onValueChange: (value: string) => void
-}
-const TextEditor: FC<TextEditorProps> = (props) => {
-  const { value, onValueChange, onChange, ...inputProps } = props
-  return (
-    <input
-      type="text"
-      value={value ?? ''}
-      onChange={(e) => {
-        onChange?.(e)
-        onValueChange(e.target.value)
-      }}
-      className="w-full h-full border-none bg-transparent focus:outline-none focus:ring-[2px] ring-brand px-4"
-      autoFocus
-      {...inputProps}
-    />
-  )
-}
-
-const RHFCellEditor = (props: CustomCellEditorProps) => {
-  const { colDef, node, form } = props
-  const fieldName = `${colDef.field}`
-
-  return (
-    <Controller
-      control={form.control}
-      name={`childs.${node.rowIndex}.${fieldName}`}
-      render={({ field }) => (
-        <TextEditor
-          value={field.value}
-          onValueChange={(val) => {
-            field.onChange(val)
+      <FormProvider {...form}>
+        <AgGridReact
+          rowData={dataSource.fields}
+          columnDefs={[
+            {
+              field: 'summa',
+              headerName: 'summa',
+              editable: true,
+              valueFormatter: (params) => formatNumber(params.value),
+              cellEditor: NumberEditor
+            },
+            {
+              field: 'schet',
+              headerName: 'Schet',
+              editable: true,
+              cellEditor: TextEditor
+            }
+          ]}
+          defaultColDef={{
+            valueParser: (params) => params.newValue
           }}
-          onBlur={() => {
-            props.stopEditing()
+          containerStyle={{
+            height: '100%'
+          }}
+          context={{
+            arrayName: 'childs'
           }}
         />
-      )}
-    />
+      </FormProvider>
+    </div>
   )
 }
 
