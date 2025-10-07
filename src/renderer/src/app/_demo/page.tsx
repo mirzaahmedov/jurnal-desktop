75a8d59d4d659c2ac12d8bb26e6bf8fc7c5e2087
Mirzaahmedov Bekzod <mirzaahmedov.dev@gmail.com>
2025-10-07 10:21:54 +0500

    add document loading animation

--- src/renderer/src/app/_demo/page.tsx
+++ src/renderer/src/app/_demo/page.tsx
@@ -1,9 +1,32 @@
-import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react'
+import { useTranslation } from 'react-i18next'
 
-ModuleRegistry.registerModules([AllCommunityModule])
+import {
+  DialogContent,
+  DialogHeader,
+  DialogOverlay,
+  DialogTitle,
+  DialogTrigger
+} from '@/common/components/jolly/dialog'
 
 const DemoPage = () => {
-  return <div className="flex-1"></div>
+  const { t } = useTranslation()
+  return (
+    <DialogTrigger isOpen>
+      <DialogOverlay>
+        <DialogContent closeButton={false}>
+          <DotLottieReact
+            autoplay
+            loop
+            src="/lotties/finding-documents.json"
+          />
+          <DialogHeader>
+            <DialogTitle className="text-center">{t('document_processing')}</DialogTitle>
+          </DialogHeader>
+        </DialogContent>
+      </DialogOverlay>
+    </DialogTrigger>
+  )
 }
 
 export default DemoPage
